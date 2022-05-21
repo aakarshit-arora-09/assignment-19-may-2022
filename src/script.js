d57188367651367e2ca8e7c3e9c6fa15c9b19635
Aakarshit Arora <aakarshit.arora@sprinklr.com>
2022-05-21 16:07:55 +0530

    added key functionality and some design changes

--- src/script.js
+++ src/script.js
@@ -4,25 +4,30 @@ const list = document.querySelector(".list");
 const activeImage=document.querySelector(".cur-image");
 const activeImageName = document.querySelector(".cur-img-name");
 
+document.addEventListener("keydown",(e)=>{handleUpDown(e)});
+
+//fetch data from json
 const fetchData = async()=>{
     const response = await fetch("./src/data.json");
     const obj = await response.json();
     return obj;
 }
 
+//changing the active image
 const handleClick = (target)=>{
     if(active)
         active.classList.remove("active");
     target.classList.add("active");
     active=target;
-    const id=target.id;
-    activeImage.src=data[id].previewImage;
-    activeImageName.value=data[id].title;
+    changeActiveimage();
 }
 
 const populateList = (data)=>{
+    //make first image in list as active image
     activeImage.src=data[0].previewImage;
     activeImageName.value=data[0].title;
+
+    //populate list with images and names
     data.forEach((item,index)=>{
         const thumbnail = document.createElement("img");
         const p= document.createElement("p");
@@ -39,12 +44,36 @@ const populateList = (data)=>{
         div.append(p);
         list.append(div);
     })
+
+    //adding event listeners for clicks on all list items;
     document.querySelectorAll(".list-item").forEach((item)=>{
     item.addEventListener("click",(e)=>{
         handleClick(item);
     })
 })}
 
+//up and down functionality
+const handleUpDown = (e)=>{
+    let id = active.id;
+
+    if(e.code==="ArrowUp" && id>0)
+        id--;
+    
+    if(e.code==="ArrowDown" && id<data.length-1)
+        id++;
+
+    active.classList.remove("active");
+    active=document.getElementById(id);
+    active.classList.add("active");
+    changeActiveimage();
+}
+
+//change the image which is active
+const changeActiveimage = ()=>{
+    activeImage.src=data[active.id].previewImage;
+    activeImageName.value=data[active.id].title;
+}
+//get data in global variable
 const getData = async()=>{
     data = await fetchData();
     populateList(data);
